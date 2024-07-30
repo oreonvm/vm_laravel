0b3ba7f0ad45c7df6a679c0c4dea37f766eacca4
oreonvm <159271904+oreonvm@users.noreply.github.com>
2024-07-30 18:36:41 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,15 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use App\Mail\TestMail;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Illuminate\Validation\Rules\Password as RulesPassword;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 //use App\Providers\RouteServiceProvider;
 
@@ -38,7 +47,7 @@ use App\Models\Post;
 Route::get('/', function () {
     return view('home');
 });
-// ===>> СОЗДАЕМ МАРШРУТЫ для ГЛАВНОЙ СТРАНИЦЫ и других страниц =======
+// ===>> Building routes for website - СОЗДАЕМ МАРШРУТЫ для ГЛАВНОЙ СТРАНИЦЫ и других страниц =======
 Route::get('/', [HomeController::class, 'index'])->name('home.index');
 Route::get('/search', [SearchController::class, 'index'])->name('search');
 Route::get('/test', [HomeController::class, 'test'])->name('home.test');
@@ -64,7 +73,7 @@ Route::get('/user/alert', [UserController::class, 'alert']);
 //     Route::post('/login_oreho', [MainController::class, 'loginOreho'])->name('login');
 // });
 
-// ==================>> ДЛЯ НЕАВТОРИЗОВАННОГО user ИЛИ  ГОСТЯ  =================
+// ==================>> For unauthorized user  =================
 Route::group(['prefix' => '/'], function () {
     Route::get('/register', [UserController::class, 'registration'])->name('register.registration');
     Route::post('/register', [UserController::class, 'store'])->name('register.store');
@@ -72,38 +81,74 @@ Route::group(['prefix' => '/'], function () {
     Route::post('/login', [UserController::class, 'login'])->name('login');
 });
 
-// ==================>> ДЛЯ АВТОРИЗОВАННОГО user  ===========================
+// ==================>> For authorized user ===========================
 Route::get('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 
-// ===============>> МАРШРУТЫ  для АДМИНА  =============================
+/**
+ * ===============>> Reset Password user =========================================
+ */
+// =================> FIRST ROUTE =======================
+Route::get('/forgot-password', function () {
+    return view('user.forgot-password', ['title' => 'Reset password']);
+})->middleware('guest')->name('password.request');
+
+// ========== SECOND ROUTE ==>> Processing form submission to send link to email
+Route::post('/forgot-password', [UserController::class, 'sendResetLink'])->name('password.email');
+
+/**
+ *   THIRD ROUTE ===>> WHERE user AFTER RECEIVING email AND CLICKING ON THE LINK
+* TO ENTER A NEW PASSWORD WILL BE REFERRED TO THE PASSWORD RESET FORM
+ */
+Route::get('/reset-password/{token}', function (string $token) {
+    $current_time = time();
+    $user = User::where('token', $token)->first();
+    // dd($user);
+    $tstamp_current = $user['tstamp'];
+    // if ($tstamp_current = 0)  abort(404, 'This page not found! ');
+    // ==================> as a possible option ===================================
+    if ($token && $current_time - $tstamp_current > 320) {
+        abort(404, 'This page not found! ');
+    }
+    //==============================================================================
+    return view('user.reset-password', compact('user', 'token', 'tstamp_current', 'current_time'), ['title' => 'Update your password', 'token' => $token]);
+})->middleware('guest')->name('password.reset');
+
+/**
+ *  FOURTH ROUTE ===>> Form processing AFTER ENTERING A NEW PASSWORD
+ */
+Route::post('/reset-password', [UserController::class, 'newPassw'])->name('password.update');
+
+//====================  End Reset Password user =======================================
+
+// ===============>> Routes for Admin panel  =============================
 Route::group(['prefix' => 'oreho'], function () {
     Route::get('/login_oreho', [UserController::class, 'loginAdmin'])->name('login.oreho');
     Route::post('/', [UserController::class, 'loginOreho'])->name('login_oreho');
     Route::get('/logout', [UserController::class, 'logoutAdmin'])->name('logoutAdmin');
     // });
-    // ======>> для всех файлов находящихся в АДМИНКЕ =================
+    // ======>> For all files in the admin area =================
     Route::get('/', [MainController::class, 'index']);
     Route::resource('/categories', CategoryController::class);
     Route::resource('/tags', TagController::class);
     Route::resource('/users', AdminUserController::class);
-    // ===========================>>  ПОСТЫ  для АДМИНКИ  ===================================
+    // ===========================>>  Posts for Admin area ===================================
     Route::resource('/posts', AdminPostController::class);
     Route::get('/posts/create', [AdminPostController::class, 'create'])->name('postsAdmin.create');
     Route::post('/posts', [AdminPostController::class, 'store'])->name('postsAdmin.store');
     Route::get('/posts/{post}/edit/', [AdminPostController::class, 'edit'])->name('postsAdmin.edit');
     Route::put('/posts/{post}', [AdminPostController::class, 'update'])->name('postsAdmin.update');
     Route::delete('/posts/{post}', [AdminPostController::class, 'destroy'])->name('postsAdmin.destroy');
-    //=====================>> USERS для АДМИНКИ  ======================================
+    //=====================>> USERS for Admin area ======================================
     Route::delete('/users/{user}', [AdminUserController::class, 'destroy'])->name('users.destroy');
     // Route::get('/users/{user}/edit/', [AdminUserController::class, 'edit'])->name('users.edit');
     // Route::put('/users/{user}', [AdminUserController::class, 'update'])->name('users.update');
-    //  =====================>>  КАТЕГОРИИ для АДМИНКИ  ===================================
+    //  =====================>>  Categories for Admin area  ===================================
     Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
     Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
     Route::get('/categories/{category}/edit/', [CategoryController::class, 'edit'])->name('categories.edit');
     Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
     Route::delete('/categories/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
-    // ===========================>> ТЕГИ для АДМИНКИ  ====================================
+    // ===========================>> Tags for Admin area ====================================
     Route::get('/tags/create', [TagController::class, 'create'])->name('tags.create');
     Route::post('/tags', [TagController::class, 'store'])->name('tags.store');
     Route::get('/tags/{tag}/edit/', [TagController::class, 'edit'])->name('tags.edit');
@@ -111,7 +156,7 @@ Route::group(['prefix' => 'oreho'], function () {
     Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
 });
 // =====================================================================
-// ====================>> ПОСТЫ НА САМОМ САЙТЕ  ========================================
+// ====================>> Posts on website  ========================================
 
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('/', [PostController::class, 'store'])->name('posts.store');
@@ -133,6 +178,7 @@ Route::get('/posts/post/{id?}', [PostController::class, 'show'])->name('post');
  *   ====>> ЕСЛИ НА САЙТЕ ВОЗМОЖНЫ КАКИЕ-ТО НЕСУЩЕСТВУЮЩИЕ ЗАПРОСЫ
  *  ТО МОЖНО СДЕЛАТЬ ТАК, ЧТОБЫ ВСЕ ЭТИ НЕСУЩЕСТВУЮЩИЕ ЗАПРОСЫ
  * ПЕРЕАДОЕСОВЫВАЛИСЬ НА НАПРИМЕР ГЛАВНУЮ СТРАНИЦУ с ПОМ ЙУНКЦИИ fullback
+ *  REDIRECTION on the page - NOT FOUND PAGE 
  */
 Route::fallback(function () {
     //return redirect('home');
