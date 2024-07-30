6132a3092ed788fb05b2ad6e2e90b563d7536062
oreonvm <159271904+oreonvm@users.noreply.github.com>
2024-07-30 19:03:16 +0300

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 @section('footer')
     <footer>
-        <div class="col-xs-12 col-sm-12 col-md-8">
+         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
             <div class="copyright"> &copy; {{ date('Y') }} {{ 'vm_laravel' }}</div>
         </div>
     </footer>
